d5f264e267de3d73e3be459fa5174c1cd7bafa6a
mimski <44443424+mimski@users.noreply.github.com>
2018-11-18 20:21:21 +0200

    Add files via upload

---
+++ C# Judge Tasks/Крачкомер/Walkmeter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Walkmeter
+{
+    static void Main()
+    {
+        int stepsInMiles = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("{0}", stepsInMiles * 1609);
+    }
+}

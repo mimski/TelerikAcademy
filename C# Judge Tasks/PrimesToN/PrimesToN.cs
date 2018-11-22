35641cab9325b68f52268e67005986d7a4158af6
mimski <44443424+mimski@users.noreply.github.com>
2018-11-22 10:49:54 +0200

    Add files via upload

---
+++ C# Judge Tasks/PrimesToN/PrimesToN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class PrimesToN
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool isPrime = false;
+
+        for (int i = 1; i <= n; i++)
+        {
+            isPrime = true;
+            for (int j = 1; j <= n; j++)
+            {
+                if (i % j == 0 && i != j && j != 1)
+                {
+                    isPrime = false;
+                    break;
+                }
+                else
+                {
+                    isPrime = true;
+                }
+            }
+            if (isPrime)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}

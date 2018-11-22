fccead665ab3ff3467a478e0fd3ec093c95827fb
mimski <44443424+mimski@users.noreply.github.com>
2018-11-22 20:18:02 +0200

    Add files via upload

---
+++ ExamProblems/EvenDifferences/EvenDifferencesV2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class EvenDifferencesV2
+{
+    static void Main()
+    {
+        //read input
+        var sequence = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+        long sumOfEvenDiff = 0;
+
+        for (int i = 1; i < sequence.Length; i++)
+        {
+            // find the abs difference
+            long absDiff = Math.Abs(sequence[i] - sequence[i - 1]);
+
+            if (absDiff % 2 == 0) // if even
+            {
+                i++; //make even jump
+                sumOfEvenDiff += absDiff; // sum abs difference
+            }
+        }
+        Console.WriteLine(sumOfEvenDiff);
+    }
+}

c4df823c2e328fce13456c636f89b5ffeadd4699
mimski <44443424+mimski@users.noreply.github.com>
2018-11-25 21:05:40 +0200

    Add files via upload

---
+++ ExamProblems/Text to Number/TextToNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class TextToNumber
+{
+    static void Main()
+    {
+        int m = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+
+        long result = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char ch = text[i];
+
+            if (ch == '@')
+            {
+                break;
+            }
+            else if ('0' <= ch && ch <= '9')
+            {
+                result *= (ch - '0');
+            }
+            else if ('a' <= ch && ch <= 'z')
+            {
+                result += (ch - 'a');
+            }
+            else if ('A' <= ch && ch <= 'Z')
+            {
+                result += (ch - 'A');
+            }
+            else
+            {
+                result %= m;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ ExamProblems/Text to Number/TextToNumberV2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class TextToNumberV2
+{
+    static void Main()
+    {
+        int m = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+
+        text = text.ToLower();
+
+        long result = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char ch = text[i];
+            if (ch == '@')
+            {
+                break;
+            }
+            else if('0' <= ch && ch <= '9')
+            {
+                result *= (ch - '0');
+            }
+            else if ('a' <= ch && ch <= 'z')
+            {
+                result += (ch - 'a');
+            }
+            else
+            {
+                result %= m;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}

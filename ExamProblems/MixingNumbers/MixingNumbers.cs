a1c68342b120ffc5e43a0f59d49a1ab4bc485a76
mimski <44443424+mimski@users.noreply.github.com>
2018-11-26 22:45:22 +0200

    Add files via upload

---
+++ ExamProblems/MixingNumbers/MixingNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MixingNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string mixLine = "";
+        string subLine = ""; 
+
+        int ab = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n - 1; i++)
+        {
+            int cd = int.Parse(Console.ReadLine());
+
+            mixLine += string.Format("{0} ", (ab % 10) * ((cd / 10) % 10));
+            subLine += string.Format("{0} ", Math.Abs(ab - cd));
+
+            ab = cd;
+        }
+
+        Console.WriteLine(mixLine);
+        Console.WriteLine(subLine);
+    }
+}
---
+++ ExamProblems/MixingNumbers/MixingNumbersV2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MixingNumbersV2
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] mixArr = new int[n - 1];
+        int[] subArr = new int[n - 1];
+
+        for (int i = 0; i < mixArr.Length; i++)
+        {
+            mixArr[i] = (nums[i] % 10) * ((nums[i + 1] / 10) % 10);
+            subArr[i] = Math.Abs(nums[i] - nums[i + 1]);
+        }
+        Console.WriteLine(string.Join(" ", mixArr));
+        Console.WriteLine(string.Join(" ", subArr));
+    }
+}

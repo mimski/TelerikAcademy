c3deb3cdec3e7ad065a64b045ceb2ddde65f2f56
mimski <44443424+mimski@users.noreply.github.com>
2018-11-26 19:32:31 +0200

    Add files via upload

---
+++ ExamProblems/MythicalNumbers/MythicalNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MythicalNumbers
+{
+    static void Main()
+    {
+        int abc = int.Parse(Console.ReadLine());
+        int a = (abc / 100) % 10;
+        int b = (abc / 10) % 10;
+        int c = abc % 10;
+
+        double result;
+
+        if (c == 0)
+        {
+            result = a * b;
+        }
+        else if (c <= 5)
+        {
+            result = a * b / (double)c;
+        }
+        else
+        {
+            result = (a + c) * c;
+        }
+        Console.WriteLine($"{result:F2}");
+    }
+}
---
+++ ExamProblems/MythicalNumbers/MythicalNumbersV2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MythicalNumbersV2
+{
+    static void Main()
+    {
+        string abc = Console.ReadLine();
+        int a = abc[0] - '0';
+        int b = abc[1] - '0';
+        int c = abc[2] - '0';
+
+        double result = 0;
+        if (c == 0)
+        {
+            result = a * b;
+        }
+        else if (c <= 5)
+        {
+            result = (a * b) / (double)c;
+        }
+        else if (c > 5)
+        {
+            result = (a + b) * c;
+        }
+        Console.WriteLine("{0:F2}", result);
+    }
+}

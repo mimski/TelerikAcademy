1366a63d24efffc761dfa0371334b6c0a7434243
mimski <44443424+mimski@users.noreply.github.com>
2018-11-26 21:50:59 +0200

    Add files via upload

---
+++ ExamProblems/HiddenMessage/HiddenMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class HiddenMessage
+{
+    static void Main()
+    {
+        string hiddenMessage = "";
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "end")
+            {
+                break;
+            }
+
+            int i = int.Parse(input);
+            int s = int.Parse(Console.ReadLine());
+
+            string subtitles = Console.ReadLine();
+
+            if (i < 0)
+            {
+                i += subtitles.Length;
+            }
+
+            for (; i < subtitles.Length && i >= 0; i += s)
+            {
+                hiddenMessage += subtitles[i];
+            }
+        }
+        Console.WriteLine(hiddenMessage);
+    }
+}
---
+++ ExamProblems/HiddenMessage/HiddenMessageV2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class HiddenMessageV2
+{
+    static void Main()
+    {
+        string hiddenMessage = "";
+
+        string input = Console.ReadLine();
+       
+        while (input != "end")
+        {
+            int i = int.Parse(input);
+            int s = int.Parse(Console.ReadLine());
+            string subtitles = Console.ReadLine();
+
+            if (i < 0) //if i is a negative int (-i)
+            {
+                i += subtitles.Length; 
+            }
+
+            while (i >= 0 && i < subtitles.Length)
+            {
+                hiddenMessage += subtitles[i];
+                i += s; //if (+i) goes right, if (-i) goes left
+            }
+            input = Console.ReadLine();
+        }
+        Console.WriteLine(hiddenMessage);
+    }
+}

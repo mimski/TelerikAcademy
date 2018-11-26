4fd6bfd8ed8685f79b28886d2291fa24da749906
mimski <44443424+mimski@users.noreply.github.com>
2018-11-27 01:28:29 +0200

    Add files via upload

---
+++ ExamProblems/BirdsAndFeathers/BirdsAndFeathers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class BirdsAndFeathers
+{
+    static void Main()
+    {
+        int b = int.Parse(Console.ReadLine());
+        long f = long.Parse(Console.ReadLine());
+
+        double ave = (double)f / b;
+
+        if (b == 0)
+        {
+            ave = 0;
+        }
+        if (b % 2 == 0)
+        {
+            ave *= 123123123123;
+        }
+        if (b % 2 == 1)
+        {
+            ave /= 317;
+        }
+        Console.WriteLine("{0:F4}", ave);
+    }
+}
---
+++ ExamProblems/BirdsAndFeathers/BirdsAndFeathersV2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class BirdsAndFeathersV2
+{
+    static void Main()
+    {
+        int b = int.Parse(Console.ReadLine());
+        long f = long.Parse(Console.ReadLine());
+
+        double ave = 0;
+
+        if (b != 0)
+        {
+            ave = (double)f / b;
+
+            if (b % 2 == 0) //if even
+            {
+                ave *= 123123123123;
+            }
+            else //if (b % 2 == 1) //if odd
+            {
+                ave /= 317;
+            }
+        }
+        Console.WriteLine("{0:F4}", ave);
+    }
+}

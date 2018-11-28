658fdc5a89ab13d50fd179cf0306b02025f20fe8
mimski <44443424+mimski@users.noreply.github.com>
2018-11-28 11:22:13 +0200

    Add files via upload

---
+++ ExamProblems/3-6-9/ThreeSixNine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class ThreeSixNine
+{
+    static void Main()
+    {
+        long result = 0;
+
+        long a = long.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        long c = long.Parse(Console.ReadLine());
+
+        if (b == 3)
+        {
+            result = a + c;
+        }
+        else if (b == 6)
+        {
+            result = a * c;
+        }
+        else if (b == 9)
+        {
+            result = a % c;
+        }
+
+        if (result % 3 == 0)
+        {
+            Console.WriteLine(result / 3);
+        }
+        else
+        {
+            Console.WriteLine(result % 3);
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ ExamProblems/3-6-9/ThreeSixNineV2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ThreeSixNineV2
+{
+    static void Main()
+    {
+        long a = long.Parse(Console.ReadLine());
+        byte b = byte.Parse(Console.ReadLine());
+        long c = long.Parse(Console.ReadLine());
+
+        long r1 = 0;
+        switch (b)
+        {
+            case 3: r1 = a + c; break;
+            case 6: r1 = a * c; break;
+            case 9: r1 = a % c; break;
+        }
+
+        long r2 = r1 % 3 == 0 ? (r1 / 3) : (r1 % 3);
+
+        Console.WriteLine(r2);
+        Console.WriteLine(r1);
+    }
+}

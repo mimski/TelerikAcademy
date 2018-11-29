0750d2b9fa6883ccdcde36e7212db5ffe4e53988
mimski <44443424+mimski@users.noreply.github.com>
2018-11-29 21:03:05 +0200

    Add files via upload

---
+++ ExamProblems/CrookedDigits/CrookedDigits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class CrookedDigits
+{
+    static void Main()
+    {
+        string n = Console.ReadLine();
+        if (n.Length == 1)
+        {
+            Console.WriteLine(n);
+        }
+
+        while (n.Length > 1)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                char ch = n[i];
+                if (char.IsDigit(n[i]))
+                {
+                    int digit = int.Parse(ch.ToString());
+                    sum += digit;
+                }
+            }
+            if (sum <= 9)
+            {
+                Console.WriteLine(sum);
+            }
+            n = sum.ToString();
+        }
+    }
+}
